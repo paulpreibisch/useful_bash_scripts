c1ceee6a66c9495f6e97b542bf14148ea3886709
fire <fire@b3dmultitech.com>
2016-07-21 15:22:37 +0800

    random sound script

--- cloudflare_getzones.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TKN=your global api key
-DOMAIN=your domain
-EMAIL=your email
-curl -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
-  -H "X-Auth-Email: $EMAIL" \
-  -H "X-Auth-Key: $TKN" \
-  -H "Content-Type: application/json"
